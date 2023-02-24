1fd04c3c5e1710b960f2d4fb0068cccd2cd4576d
Prohit <udkarprohit@gmail.com>
2023-02-24 22:32:33 +0530

    Added: Udating data in database table from java

--- src/main/java/com/bridgelabz/JDBCDemoClass.java
+++ src/main/java/com/bridgelabz/JDBCDemoClass.java
@@ -19,6 +19,9 @@ public class JDBCDemoClass {
             while (rs.next())
                 System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getDouble(3) + "\t" + rs.getDate(4));
 
+            int count = stmt.executeUpdate("update employee_payroll set basic_pay = 3000000.00 where name = 'Terissa'");
+            System.out.println(count);
+
             con.close();
         } catch (Exception e) {
             System.out.println(e);
