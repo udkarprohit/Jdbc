4fcf4924b973395bf63ae31684cad8a53384ef1a
Prohit <udkarprohit@gmail.com>
2023-02-26 22:45:30 +0530

    Added: Updating salary in db using Prepared Statement

--- src/main/java/com/bridgelabz/JDBCDemoClass.java
+++ src/main/java/com/bridgelabz/JDBCDemoClass.java
@@ -1,31 +1,60 @@
 package com.bridgelabz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JDBCDemoClass {
-
-    public static void main(String[] args) {
-
+    static String url = "jdbc:mysql://localhost:3306/payroll_service";
+    static String userName="root";
+    static String password="";
+    public static void fetchData(String queryFetch) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "");
-            // here so noo is database name, root is username and password
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from employee_payroll");
-            System.out.println("ID\t" + "EmpName   " + "Salary\t" + "Date");
-            while (rs.next())
-                System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getDouble(3) + "\t" + rs.getDate(4));
-
-            int count = stmt.executeUpdate("update employee_payroll set basic_pay = 3000000.00 where name = 'Terissa'");
-            System.out.println(count);
-
-            con.close();
-        } catch (Exception e) {
-            System.out.println(e);
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.print("The error in the class -" + e);
+        }
+        try {
+            Connection connection =DriverManager.getConnection(url,userName,password);
+            Statement st = connection.createStatement();
+            System.out.println("The felting the data is started  \n");
+            ResultSet rs =st.executeQuery(queryFetch);
+            //The result we want in the table formatte so we use RS
+            //fetch the data
+            System.out.println("The result set - " + rs);
+            while (rs.next()) {
+                System.out.println(rs.getInt(1)+" "+rs.getNString(2) + "  " + rs.getDate(3)+" "+ rs.getDouble(4 ) + " "+ rs.getString(5));
+            }
+            System.out.println("Closing point of the Fetch method");
+            st.close();
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("The error at the Connection - " + e);
         }
     }
+    public static void set(){
+        try {
+            Connection connection =DriverManager.getConnection(url,userName,password);
+            // Statement st = connection.createStatement();
 
+            String query ="update employee_payroll set salary = ? where id = ? ";
+            // " insert into employee_payroll(name,salary,start) values(?,?,?)";
+            System.out.println("--------The query point for the updating ------ \n");
+            PreparedStatement st = connection.prepareStatement(query);
+            st.setDouble(1,3600000);
+            st.setInt(2,4);
+
+            int count = st.executeUpdate();
+            System.out.println("The no row affected + "+ count);
+            st.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("error - "+ e);
+        }
+
+    }
+    public static void main(String[] args) {
+        fetchData("select * from employee_payroll");
+        set();
+        fetchData("select * from employee_payroll");
+    }
 }
